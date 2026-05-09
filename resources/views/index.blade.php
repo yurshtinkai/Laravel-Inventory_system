6ccb3b75671d96811df21cf25ce273d8b1ddc81a
yurshtinkai <lourdangeloubufete17@gmail.com>
2026-05-09 23:57:15 +0800

    commit changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -239,11 +239,8 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
-            <img src="logo_inven.jpg" alt="Inventory Logo" class="logo-inventory"/>
-            <a class="navbar-brand" href="#">
-                <img src="bc.png" alt="BC Logo" class="logo-bc"/>
-                Inventory System
-            </a>
+            <img src="bc.png" alt="BC Logo" class="logo-inventory"/>
+            <a class="navbar-brand" href="#">Inventory System</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
