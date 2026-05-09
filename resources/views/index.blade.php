6086fa107bd35c42d8e208e0051118095db11723
yurshtinkai <lourdangeloubufete17@gmail.com>
2026-05-09 23:05:30 +0800

    Update navbar logo to use bc.png

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -214,7 +214,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
-            <img src="logo_inven.jpg" alt="Logo"/>
+            <img src="bc.png" alt="Logo"/>
             <a class="navbar-brand" href="#">Inventory System</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
